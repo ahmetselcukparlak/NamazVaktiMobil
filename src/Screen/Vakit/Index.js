22c43e94fd7d82e7f0eb04a2acb4a743f997503a
erenibrahim <63008059+erenibrahim@users.noreply.github.com>
2021-10-24 01:24:54 +0300

    Sayfalar arası veri aktarımı

--- src/Screen/Vakit/Index.js
+++ src/Screen/Vakit/Index.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react';
-import {Button, View} from 'react-native';
-export default function VakitScreen({ navigation }){
+import {Button, View, Text} from 'react-native';
+export default function VakitScreen({ route, navigation }){
+
+    const timeData = route.params;
+
+    //console.log(timeData);
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Tarih: {timeData[0].MiladiTarihUzun}</Text>
+             <Text>İmsak: {timeData[0].Imsak} Güneş: {timeData[0].Gunes} Öğle: {timeData[0].Ogle} İkindi: {timeData[0].Ikindi} Akşam: {timeData[0].Aksam} Yatsı: {timeData[0].Yatsi} </Text>
             <Button onPress={() => navigation.goBack()} title="Vakit Sayfasına Git" />
         </View>
     );
